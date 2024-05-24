3d79e4ec81675b452525a76af9dc299f9d169668
gloireisesa <isesagloire0@gmail.com>
2024-05-24 14:36:31 +0200

    AMÉLIORATION  DE Dog.kt

--- app/src/main/java/Data/Dog.kt
+++ app/src/main/java/Data/Dog.kt
@@ -1,7 +1,23 @@
-package com.exeple.woof
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.woof.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.exeple.woof.R
 
 /**
  * A data class to represent the information presented in the dog card
@@ -24,4 +40,3 @@ val dogs = listOf(
     Dog(R.drawable.tzeitel, R.string.dog_name_8, 7, R.string.dog_description_8),
     Dog(R.drawable.leroy, R.string.dog_name_9, 4, R.string.dog_description_9)
 )
-
